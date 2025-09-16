0523213de1fa9b636e21abaa992c00085e71ed59
aspiringluke <l.paulino@sou.unifeob.edu.br>
2025-09-16 17:14:43 -0300

    Melhorias na interatividade e no display dos dados

--- src/atividade15092025.ts
+++ src/atividade15092025.ts
@@ -1,35 +1,48 @@
 import { logar } from "./config/config.js";
 import { selectAll } from "./queries.js";
+import promptSync from "prompt-sync";
 
 async function testar(): Promise<void>
 {
+    const prompt = promptSync();
+
     console.log("Logando como administrador. . .");
     let db = logar();
     let vendas = await selectAll(db, "vendas", "*");
     let contasreceber = await selectAll(db, "contasreceber", "*");
     
-    console.log("Vendas: ", vendas);
-    console.log("Contas a receber: ", contasreceber);
+    console.log("Vendas: ");
+    console.table(vendas);
+    console.log("Contas a receber: ");
+    console.table(contasreceber);
 
-
+    prompt("Pressione ENTER para continuar. . .");
+    console.clear();
 
     console.log("Logando como operador. . .");
     db = logar();
     vendas = await selectAll(db, "vendas", "*");
     contasreceber = await selectAll(db, "contasreceber", "*");
 
-    console.log("Vendas: ", vendas);
-    console.log("Contas a receber: ", contasreceber);
+    console.log("Vendas: ");
+    console.table(vendas);
+    console.log("Contas a receber: ");
+    console.table(contasreceber);
     
-
+    prompt("Pressione ENTER para continuar. . .");
+    console.clear();
 
     console.log("Logando como vendedor. . .");
     db = logar();
     let vendasfiltradas = await selectAll(db, "vendasfiltradas", "*");
     let contasfiltradas = await selectAll(db, "contasfiltradas", "*");
 
-    console.log("Vendas filtradas: ", vendasfiltradas);
-    console.log("Contas a receber filtradas: ", contasfiltradas);
+    console.log("Vendas filtradas: ");
+    console.table(vendasfiltradas);
+    console.log("Contas a receber filtradas: ");
+    console.table(contasfiltradas);
+
+    prompt("Pressione ENTER para continuar. . .");
 
     process.exit(0);
 }
